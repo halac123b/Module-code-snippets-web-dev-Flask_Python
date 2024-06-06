58fae50388cd3dee7ad0adf8a4b9957ebcb1de44
halac123b <halac123b@gmail.com>
2024-06-06 15:19:44 +0700

    Add m_1

--- 1/app.py
+++ 1/app.py
@@ -1,1 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import flask
+
+# Init Flask server obj
+## Address __name__ đến file hiện tại nơi chứa server
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # Hàm python giúp render và return file HTML
+    ## Hàm biết tự động tìm đến folder templates
+    return render_template("index.html")
+
+
+@app.route("/file.txt")
+def response_file():
+    # Response to client a file located in a directory
+    return flask.send_from_directory("document", "file.txt")
+
+
+if __name__ == "__main__":
+    # Khởi động server, bật debug mode (nếu có lỗi sẽ popup lên màn hình)
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 <p>List of modules</p>
 <ol>
+<li>Flask basic syntax, Jinja HTML template</li>
 <li>Stream OpenAI API Responses</li>
 </ol>
